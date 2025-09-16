26aff7ee94df6b7e32a6ad1ef41c6607b22e96fd
Keith Laban <kelaban17@gmail.com>
2025-09-16 16:15:04 -0400

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,93 @@
 import * as d3 from "d3";
 import * as z from "zod";
 import React from "react";
-import SpiralHand from "./SpiralHand";
 import AgeClock from "./AgeClock";
 import DailyClock from "./DailyClock";
 import { Outlet, useOutletContext, useSearchParams } from "react-router";
-
-const countSeconds = (start?: Date) =>
-  d3.timeSecond.count(start ? start : d3.timeDay.floor(new Date()), new Date())
-
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
-    height
+    height,
   };
 }
 
-
 type ContextType = { maxRadius: number };
 
-
 const ClockParams = z.object({
-  unwind: z.stringbool().default(true)
-})
+  unwind: z.stringbool().default(true),
+  minuteColor: z.string().default("#FFD63A"),
+  hourColor: z.string().default("#6DE1D2"),
+  secondColor: z.string().default("#F75A5A")
+});
 
 export function DailyClockView() {
   const { maxRadius } = useOutletContext<ContextType>();
-  const [searchParams] = useSearchParams()
-  const params = ClockParams.parse({ unwind: searchParams.get("unwind") || undefined })
+  const [searchParams] = useSearchParams();
+  const params = ClockParams.parse({
+    unwind: searchParams.get("unwind") || undefined,
+    minuteColor: searchParams.get("m") || undefined,
+    hourColor: searchParams.get("h") || undefined,
+    secondColor: searchParams.get("s") || undefined
+  });
 
-  return <DailyClock unwind={params.unwind} maxRadius={maxRadius} />
+  return <DailyClock maxRadius={maxRadius} {...params} />;
 }
 
 const AgeParams = z.object({
   b: z.array(
-    z.tuple([
-      z.iso.date().pipe(z.coerce.date()),
-      z.string().default("white")]
-    )
-  )
-})
+    z.tuple([z.iso.date().pipe(z.coerce.date()), z.string().default("white")])
+  ),
+});
 
 export function AgeClockView() {
   const { maxRadius } = useOutletContext<ContextType>();
-  const [searchParams] = useSearchParams()
-  const b = searchParams.getAll("b").map((e) => e.split("|"))
-  const params = AgeParams.parse({ b })
+  const [searchParams] = useSearchParams();
+  const b = searchParams.getAll("b").map((e) => e.split("|"));
+  const params = AgeParams.parse({ b });
 
   return (
     <React.Fragment>
-      {params.b.map(p => <AgeClock key={p[0].toString()} birthday={p[0]} color={p[1]} maxRadius={maxRadius} />)}
-
+      {params.b.map((p) => (
+        <AgeClock
+          key={p[0].toString()}
+          birthday={p[0]}
+          color={p[1]}
+          maxRadius={maxRadius}
+        />
+      ))}
     </React.Fragment>
-  )
+  );
 }
 
 export function App() {
-  const [windowDimensions, setWindowDimensions] = React.useState(getWindowDimensions());
-  const maxRadius = Math.min(windowDimensions.height, windowDimensions.width) / 2
-  const [searchParams] = useSearchParams()
-  const background = searchParams.get("bg") || "white"
+  const [windowDimensions, setWindowDimensions] = React.useState(
+    getWindowDimensions()
+  );
+  const maxRadius =
+    Math.min(windowDimensions.height, windowDimensions.width) / 2;
+  const [searchParams] = useSearchParams();
+  const background = searchParams.get("bg") || "white";
 
   React.useEffect(() => {
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
     }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <div style={{ background, width: "100%", height: "100vh", display: "flex" }}>
-      <svg width={maxRadius * 2} height={maxRadius * 2} style={{ background: "none", display: "block", margin: "auto" }}>
+    <div
+      style={{ background, width: "100%", height: "100vh", display: "flex" }}
+    >
+      <svg
+        width={maxRadius * 2}
+        height={maxRadius * 2}
+        style={{ background: "none", display: "block", margin: "auto" }}
+      >
         <Outlet context={{ maxRadius }} />
       </svg>
     </div>
